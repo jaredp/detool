5752892ec86a4bb306a7367b63de8d145ff0d297
Roger Zurawicki <zurawiki@users.noreply.github.com>
2023-05-07 22:16:57 -0400

    style: Add sidebar active state, add inter

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,32 @@ import { Sidebar } from "./components/Sidebar";
 import { Company } from "./models/Company";
 import { Post } from "./models/Post";
 
-
 const Pages: [string, React.ReactElement][] = [
   ["Person", <TableAndModalPage model={Person} />],
   ["Company", <TableAndModalPage model={Company} />],
   ["Post", <TableAndModalPage model={Post} />],
-]
+];
 
 export default function App() {
-  const [selectedPage, setSelectedPage] = React.useState<typeof Pages[number]>(Pages[0]);
+  const [selectedPageIndex, setSelectedPageIndex] = React.useState<
+    number
+  >(0);
 
   return (
     <div className="flex flex-col md:h-screen">
       <Navbar />
 
       <div className="md:flex-fit md:flex md:flex-grow md:flex-row md:overflow-y-auto">
-        <Sidebar tabs={Pages} label={p => p[0]} onClick={(page) => {
-          setSelectedPage(page);
-        }} />
+        <Sidebar
+          selectedPageIndex={selectedPageIndex}
+          tabs={Pages}
+          label={(p) => p[0]}
+          onClick={(pageIndex) => {
+            setSelectedPageIndex(pageIndex);
+          }}
+        />
         <div className="flex-grow overflow-y-auto p-4">
-          { selectedPage[1] }
+          {Pages[selectedPageIndex]}
         </div>
       </div>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,10 @@
 // @ts-check
-
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
   plugins: [require('flowbite/plugin')],
 };
 module.exports = config;
